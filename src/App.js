bad159afb1e2b3028c851524bb29bbb319e338c6
vi-n-a-y <balavinay9063@gmail.com>
2025-01-03 00:24:43 +0530

    tic-tac-toe game

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import React, {useState} from 'react';
-import './App.css';
-import Board from './components/Board';
+import React from "react";
+import "./App.css";
+import Board from "./components/Board";
 
 function App() {
   return (
-    <div className="App">
-<Board/>
- 
+    <div>
+      <Board />
     </div>
   );
 }
